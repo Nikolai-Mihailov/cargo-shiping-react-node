ac78cecc28b2398908000f4846e31386ad6c8884
Nikolai Mihailov <nikolai.mihailov92@gmail.com>
2021-06-04 07:15:17 +0300

    Frontend home page and nav improvements

--- pages/index.js
+++ pages/index.js
@@ -9,39 +9,42 @@ export default function Home() {
 
   return (
     <React.Fragment>
+      <Grid>
+        <Image src="/static/assets/img/lastnav.jpg" layout="responsive" height={100} width={400} priority />
+      </Grid>
       <Grid container direction="column">
         <Grid item>
           <Grid container justify="space-evenly" alignItems="center" direction="row">
-
             <Grid item>
-              <div>
-                <Image src="/static/assets/img/navImage.jpg" height="300px" width="1000px" quality="100" />
-                <Typography variant="h2">
-                  Безплатен тестов период
-                </Typography>
-              </div>
-              <Grid container>
-                <Grid item>
-                  <Button variant="contained" color="primary">Регистрация</Button>
-                </Grid>
+              <Image src="/static/assets/img/nav3.png" height="500" width="700" quality="100" />
+            </Grid>
+            <Grid item>
+              <Typography variant="h2">text area</Typography>
+
+              <Grid item>
+                <Button variant="contained">First Button</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="outlined">Second Button</Button>
+              </Grid>
+
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container justify="space-evenly" alignItems="center" direction="row">
+            <Grid item>
+              <Typography variant="h2">text area</Typography>
+              <Grid container >
+                <Typography>lorem </Typography>
               </Grid>
             </Grid>
-          </Grid>
-        </Grid>
-
-        <Grid item>{/* Services Block */}
-          <Grid container direction="row">
             <Grid item>
-              <Typography variant="h4">Услуги</Typography>
-            </Grid>
-            <Grid item>
-              <Image src="/static/assets/img/favpng_logistics-management-inventory-business.png" width="300px" height="300px" quality="100" />
-
+              <Image src="/static/assets/img/nav3.png" height="500" width="700" quality="100" />
             </Grid>
           </Grid>
         </Grid>
-      </Grid> {/*  Main Grid */}
-
+      </Grid>
     </React.Fragment>
   )
 }
